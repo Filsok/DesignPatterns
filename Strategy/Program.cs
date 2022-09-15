1a93d7c07dc6f558c94af666a7c081f50a264378
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-09-15 19:58:27 +0200

    Initial commit. Strategy design pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,14 @@
+﻿using Strategy;
+
+Console.WriteLine("Hello, World!");
+
+var strategy = new BikeStrategy();
+
+var map = new Map(strategy);
+
+var start = new Coordinate();
+var end = new Coordinate(); 
+
+map.CreateRoute(start, end);
+
+Console.WriteLine();
