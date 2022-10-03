c0e6e08bfe176b6f5fe28c7936c4ed4ef994c35e
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-10-03 19:22:50 +0200

    Builder simple version

---
+++ Builder/Program.cs
@@ -0,0 +1,15 @@
+﻿using Builder;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new InvoiceBuilder();
+
+builder.SetVendor("Google..");
+builder.SetVendee("Vendee");
+builder.SetInvoiceNumber("111222333");
+builder.SetDate(DateTime.Now);
+builder.SetLineItems(new List<string>() { "LineItem1", "LineItem2", "LineItem3" });
+
+var invoice = builder.Build();
+
+Console.WriteLine($"{invoice.Vendor}\n{invoice.Vendee}");
