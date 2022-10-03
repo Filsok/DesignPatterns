3e591576191dc2f9731624603623842262e81932
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-10-03 20:14:41 +0200

    Abstract Factory initiate

---
+++ AbstractFactory/MacFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class MacFactory : IUIElementFactory
+    {
+        public IButton CreateButton()
+        {
+            Console.WriteLine($"CreateButton Mac");
+            return new MacButton();
+        }
+
+        public ITextBox CreateTextBox()
+        {
+            Console.WriteLine($"CreateTextBox Mac");
+            return new MacTextBox();
+        }
+    }
+}
