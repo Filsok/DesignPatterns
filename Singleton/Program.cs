a2c383967223c0e768db2e715d568c6aa1bbf016
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-10-07 20:15:16 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,8 @@
+﻿using Singleton;
+
+var cfg = Configuration.GetInstance();
+var cfg2= Configuration.GetInstance();
+
+var equals = ReferenceEquals(cfg,cfg2);
+
+Console.WriteLine($"configurations are equals: {equals}");
