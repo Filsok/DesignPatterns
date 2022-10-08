079312e95741245912b6b5524dbbeb0221eefbb1
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-10-08 08:38:33 +0200

    Mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,9 @@
+﻿using Mediator;
+
+Button submitButton = new Button();
+Checkbox clientType = new Checkbox();
+
+new RegisterClientView(clientType, submitButton);       //wow. But it works...
+
+submitButton.Click();
+clientType.Select();
