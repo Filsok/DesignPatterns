813e4f9a3d9f9abfc32bf4a8955fc532a2770dd9
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-10-23 12:57:08 +0200

    Decorator +State

---
+++ Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Decorator;
+
+var pizzaBase = new MediumPizza();
+
+var mediumPizzaWithCheese = new CheesePizzaDecorator(pizzaBase);
+Console.WriteLine($"mediumPizzaWithCheese price: {mediumPizzaWithCheese.CalculatePrice()}");
+
+var mediumPizzaWithCheeseAndSalami = new SalamiPizzaDecorator(mediumPizzaWithCheese);
+Console.WriteLine($"mediumPizzaWithCheeseAndSalami price: {mediumPizzaWithCheeseAndSalami.CalculatePrice()}");
+
+var mediumPizzaWithCheeseAndDoubleSalami = new SalamiPizzaDecorator(mediumPizzaWithCheeseAndSalami);
+Console.WriteLine($"mediumPizzaWithCheeseAndDoubleSalami price: {mediumPizzaWithCheeseAndDoubleSalami.CalculatePrice()}");
---
+++ State/CardInserted.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class CardInserted : State
+    {
+        public CardInserted(Context context) : base(context)
+        {
+        }
+
+        public override void EjectCard()
+        {
+            Console.WriteLine("Card ejected.");
+            //_context.ChangeState(NoCard);
+        }
+
+        public override void InsertCard()
+        {
+            Console.WriteLine("You have already inserted card.");
+        }
+
+        public override void InsertPin(int pin)
+        {
+            if (pin == 8888)
+            {
+                Console.WriteLine("Correct PIN inserted!");
+                //_context.ChangeState(PinInserted);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect PIN inserted!");
+                Console.WriteLine("Card ejected.");
+                //_context.ChangeState(CardEjected);
+            }
+        }
+
+        public override void WithdrawCash(int amount)
+        {
+            Console.WriteLine("Insert PIN first.");
+        }
+    }
+}
