44710d7b9baf3bdd9ed744bf5fc2d16f931028b4
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-11-02 09:05:54 +0100

    Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,11 @@
+﻿using Proxy;
+
+YoutubeService youtubeService = new YoutubeService();
+
+youtubeService.GetVideo(10);
+youtubeService.GetVideo(10);
+
+Console.WriteLine("\nNow proxy:");
+ProxyYoutubeService proxyYoutubeService = new ProxyYoutubeService(youtubeService);
+proxyYoutubeService.GetVideo(10);
+proxyYoutubeService.GetVideo(10);
---
+++ Proxy/ProxyYoutubeService.cs
@@ -0,0 +1,31 @@
+﻿namespace Proxy
+{
+    public class ProxyYoutubeService : IYoutubeService
+    {
+        private YoutubeService _youtubeService;
+        private Dictionary<int, byte[]> _cache = new Dictionary<int, byte[]>();
+
+        public ProxyYoutubeService(YoutubeService youtubeService)
+        {
+            _youtubeService = youtubeService;
+        }
+
+        public byte[] GetVideo(int videoId)
+        {
+            Console.WriteLine($"ProxyYoutubeService getting videoId");
+
+            byte[] cashedVideo;
+            if (_cache.TryGetValue(videoId, out cashedVideo))
+            {
+                Console.WriteLine($"Getting form cache {videoId}");
+                return cashedVideo;
+            }
+            else
+            {
+                var video = _youtubeService.GetVideo(videoId);
+                _cache.Add(videoId, video);
+                return video;
+            }
+        }
+    }
+}
